7a0248024ae0da6369fe68ba6b508d95980fded7
coderzc <zc1217zc@126.com>
2018-04-10 11:43:57 +0800

    init1

--- src/main/java/com/ok/okhelper/OkhelperApplication.java
+++ src/main/java/com/ok/okhelper/OkhelperApplication.java
@@ -2,8 +2,12 @@ package com.ok.okhelper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@ComponentScan
+@MapperScan(basePackages = {"com.ok.okhelper.dao"})
 public class OkhelperApplication {
 
     public static void main(String[] args) {
