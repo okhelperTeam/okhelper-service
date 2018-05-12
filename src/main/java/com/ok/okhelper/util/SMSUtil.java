87aa81c543314022615ed875e2b25eee471c220f
coderzc <zc1217zc@126.com>
2018-05-13 04:06:00 +0800

    短信手机号格式验证

--- src/main/java/com/ok/okhelper/util/SMSUtil.java
+++ src/main/java/com/ok/okhelper/util/SMSUtil.java
@@ -2,12 +2,14 @@ package com.ok.okhelper.util;
 
 import com.github.qcloudsms.SmsSingleSender;
 import com.github.qcloudsms.SmsSingleSenderResult;
+import com.ok.okhelper.exception.IllegalException;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONException;
 
 import javax.xml.ws.http.HTTPException;
 import java.io.IOException;
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 /*
 * @Author zhangxin_an 
@@ -21,8 +23,15 @@ public class SMSUtil {
 
     private static String APPKEY=PropertiesUtil.getProperty("sms.appkey");
 
+    public static final String REGEX_MOBILE = "^((17[0-9])|(14[0-9])|(13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$";
+
     public static void sendSMSCode(String phoneNumber,String captcha) {
         try {
+
+            if(!Pattern.matches(REGEX_MOBILE, phoneNumber)){
+                throw new IllegalException("手机号格式不正确");
+            }
+
             SmsSingleSender ssender = new SmsSingleSender(APPID, APPKEY);
             SmsSingleSenderResult result = ssender.send(0, "86", phoneNumber,
                     "尊敬的OK帮用户，你本次的验证码为："+captcha+"，10分钟内有效请尽快完成验证。", "", "");
@@ -48,8 +57,5 @@ public class SMSUtil {
         return vcode;
     }
 
-    public static void main(String[] args) {
-        sendSMSCode("17853252520",createRandomVcode());
-    }
 }
 
